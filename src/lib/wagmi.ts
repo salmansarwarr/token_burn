c8f782188c6ffa3f50f591803993cb13e1d7aa52
Muhammad Salman Sarwar <muhammadsalmansarwar32@gmail.com>
2026-01-25 20:47:30 +0500

    fix

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,12 +1,11 @@
 import { http, createConfig } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import { sepolia } from "wagmi/chains";
 import { getDefaultConfig } from "connectkit";
 
 export const config = createConfig(
     getDefaultConfig({
-        chains: [mainnet, sepolia],
+        chains: [sepolia],
         transports: {
-            [mainnet.id]: http(),
             [sepolia.id]: http(),
         },
         walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
