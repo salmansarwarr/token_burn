d6f7b4e6be0f1b93efe12b8e2fe8494d85f6a778
Muhammad Salman Sarwar <muhammadsalmansarwar32@gmail.com>
2026-01-30 00:21:09 +0500

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,25 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { Providers } from './providers'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Armchair - Web3 Authentication',
-  description: 'Sign in with Ethereum authentication',
-}
+    title: "Nossa Rewards",
+    description:
+        "Token Distribution Plan for Nossa Rewards (NRWD) - Transparent Treasury Management with Multi-Signature Control",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
 }
