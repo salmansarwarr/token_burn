bae346f382db39b6818f6a759e073d7c4151aa20
Muhammad Salman Sarwar <muhammadsalmansarwar32@gmail.com>
2026-01-30 01:08:48 +0500

    fix

--- src/app/token-distribution/layout.js
+++
@@ -1,4 +0,0 @@
-export const metadata = {
-    title: "Nossa Rewards - Token Distribution",
-    description: "Token Distribution Plan for Nossa Rewards (NRWD) - Transparent Treasury Management with Multi-Signature Control",
-};
